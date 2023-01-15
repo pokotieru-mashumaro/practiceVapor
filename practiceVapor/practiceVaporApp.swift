c39cab4babd3929e9c9466276efa1c7a5491e91b
s1f102200299 <s1f102200299@iniad.org>
2023-01-15 13:56:08 +0900

    Initial Commit

---
+++ practiceVapor/practiceVaporApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceVaporApp.swift
+//  practiceVapor
+//
+//  Created by iniad on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct practiceVaporApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
